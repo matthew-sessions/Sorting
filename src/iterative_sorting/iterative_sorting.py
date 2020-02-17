cf840fd76c8c74fc5ce89ea15fbd37a5308143a0
Matthew <matthewlsessions@gmail.com>
2020-02-17 15:52:40 -0800

    finised MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,20 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    lenth = len(arr)
+    for i in range(0, lenth - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
+        for j in range(i + 1, lenth):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        print(arr[smallest_index], arr[cur_index])
+        cur = arr[i]
+        smallest = arr[smallest_index]
+        arr[i] = smallest
+        arr[smallest_index] = cur
 
 
     return arr
@@ -20,6 +22,17 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    lenth  = len(arr)
+    for i in range(0, lenth):
+        base_index = i
+        for j in range(0, lenth - base_index):
+            front = j + 1
+            if front < lenth - base_index:
+                if arr[j] > arr[front]:
+                    arr_min = arr[j+1]
+                    arr_max = arr[j]
+                    arr[j] = arr_min
+                    arr[j+1] = arr_max
 
     return arr
 
@@ -28,3 +41,7 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+a = [3,6,2,5,8,9,4,6,85]
+print(bubble_sort(a))
+print(a)
